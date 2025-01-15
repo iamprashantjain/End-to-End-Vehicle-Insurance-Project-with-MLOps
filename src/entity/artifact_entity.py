dfe17b933817e163565eccfbf553ab187d94b40b
iamprashant <iamprashantjain2601@gmail.com>
2025-01-15 14:03:55 +0530

    model evaluation and pusher added s3

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -11,15 +11,12 @@ class DataValidationArtifact:
     validation_status:bool
     message: str
     validation_report_file_path: str
-    
-    
+
 @dataclass
 class DataTransformationArtifact:
     transformed_object_file_path:str 
     transformed_train_file_path:str
     transformed_test_file_path:str
-    
-    
 
 @dataclass
 class ClassificationMetricArtifact:
@@ -30,4 +27,16 @@ class ClassificationMetricArtifact:
 @dataclass
 class ModelTrainerArtifact:
     trained_model_file_path:str 
-    metric_artifact:ClassificationMetricArtifact 
+    metric_artifact:ClassificationMetricArtifact
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    changed_accuracy:float
+    s3_model_path:str 
+    trained_model_path:str
+
+@dataclass
+class ModelPusherArtifact:
+    bucket_name:str
+    s3_model_path:str
